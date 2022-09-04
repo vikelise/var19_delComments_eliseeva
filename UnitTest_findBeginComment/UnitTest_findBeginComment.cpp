5f3c289338f6960200fa52ce2049ae95c7aaba38
vikelise <viktoriya.eliseeva.2002@mail.ru>
2022-09-04 11:02:45 +0300

    add tests to findBeginComment

---
+++ UnitTest_findBeginComment/UnitTest_findBeginComment.cpp
@@ -0,0 +1,85 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include"../var19_delComments_eliseeva/var19_delComments_eliseeva.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestfindBeginComment
+{
+	TEST_CLASS(UnitTestfindBeginComment)
+	{
+	public:
+		
+		TEST_METHOD(findFromBegin)
+		{
+			vector<string>text = { "void main ()", "{", "int a = 9;/*int", "k;*/", "a = 10;", "}" };
+			int numberRow = 0;
+			int numberPos = -1;
+			int indexRowBegin = 0;
+			int posBegin = -1;
+			int countFindBegin = 0;
+			int expcountFindBegin = 1;
+			int expindexRowBegin = 2;
+			int expposBegin = 8;
+
+			findBeginComment(text, numberRow, numberPos, &indexRowBegin, &posBegin, &countFindBegin);
+			Assert::AreEqual(expcountFindBegin, countFindBegin);
+			Assert::AreEqual(expindexRowBegin, indexRowBegin);
+			Assert::AreEqual(expposBegin, posBegin);
+		}
+
+		TEST_METHOD(findFromFoundComment)
+		{
+			vector<string>text = { "void main ()", "{", "    int a=9;/*int", "k;*/", "a = 10;", "/*68", "int", "char f;*/", "}" };
+			int numberRow = 2;
+			int numberPos = 8;
+			int indexRowBegin = 2;
+			int posBegin = 8;
+			int countFindBegin = 0;
+			int expcountFindBegin = 1;
+			int expindexRowBegin = 4;
+			int expposBegin = 0;
+
+			findBeginComment(text, numberRow, numberPos, &indexRowBegin, &posBegin, &countFindBegin);
+			Assert::AreEqual(expcountFindBegin, countFindBegin);
+			Assert::AreEqual(expindexRowBegin, indexRowBegin);
+			Assert::AreEqual(expposBegin, posBegin);
+		}
+
+		TEST_METHOD(beginMultilineCommSingleLine)
+		{
+			vector<string>text = { "void main()","{","int a;//int t/*int k;","/*a=10;","a=2;*/","}" };
+			int numberRow = 0;
+			int numberPos = -1;
+			int indexRowBegin = 0;
+			int posBegin = -1;
+			int countFindBegin = 0;
+			int expcountFindBegin = -1;
+			int expindexRowBegin = 0;
+			int expposBegin = -1;
+
+			findBeginComment(text, numberRow, numberPos, &indexRowBegin, &posBegin, &countFindBegin);
+			Assert::AreEqual(expcountFindBegin, countFindBegin);
+			Assert::AreEqual(expindexRowBegin, indexRowBegin);
+			Assert::AreEqual(expposBegin, posBegin);
+		}
+
+		TEST_METHOD(beginMultilineCommInConst)
+		{
+			vector<string>text = { "void main()","{","char a= '/*12;","6890 * / '","a = 10;","}" };
+			int numberRow = 0;
+			int numberPos = -1;
+			int indexRowBegin = 0;
+			int posBegin = -1;
+			int countFindBegin = 0;
+			int expcountFindBegin = -1;
+			int expindexRowBegin = 0;
+			int expposBegin = -1;
+
+			findBeginComment(text, numberRow, numberPos, &indexRowBegin, &posBegin, &countFindBegin);
+			Assert::AreEqual(expcountFindBegin, countFindBegin);
+			Assert::AreEqual(expindexRowBegin, indexRowBegin);
+			Assert::AreEqual(expposBegin, posBegin);
+		}
+	};
+}

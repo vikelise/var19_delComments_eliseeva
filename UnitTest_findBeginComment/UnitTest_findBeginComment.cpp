b3000ba4d7e222a5f369ed516a3af87e07ff4e14
vikelise <viktoriya.eliseeva.2002@mail.ru>
2022-09-10 23:59:29 +0300

    fixed incorrect expected test results in findBeginComment

--- UnitTest_findBeginComment/UnitTest_findBeginComment.cpp
+++ UnitTest_findBeginComment/UnitTest_findBeginComment.cpp
@@ -20,7 +20,7 @@ namespace UnitTestfindBeginComment
 			int countFindBegin = 0;
 			int expcountFindBegin = 1;
 			int expindexRowBegin = 2;
-			int expposBegin = 8;
+			int expposBegin = 10;
 
 			findBeginComment(text, numberRow, numberPos, &indexRowBegin, &posBegin, &countFindBegin);
 			Assert::AreEqual(expcountFindBegin, countFindBegin);
@@ -30,14 +30,14 @@ namespace UnitTestfindBeginComment
 
 		TEST_METHOD(findFromFoundComment)
 		{
-			vector<string>text = { "void main ()", "{", "    int a=9;/*int", "k;*/", "a = 10;", "/*68", "int", "char f;*/", "}" };
+			vector<string>text = { "void main ()", "{", "int a=9;/*int", "k;*/", "a = 10;", "/*68", "int", "char f;*/", "}" };
 			int numberRow = 2;
 			int numberPos = 8;
 			int indexRowBegin = 2;
 			int posBegin = 8;
 			int countFindBegin = 0;
 			int expcountFindBegin = 1;
-			int expindexRowBegin = 4;
+			int expindexRowBegin = 5;
 			int expposBegin = 0;
 
 			findBeginComment(text, numberRow, numberPos, &indexRowBegin, &posBegin, &countFindBegin);
@@ -48,13 +48,13 @@ namespace UnitTestfindBeginComment
 
 		TEST_METHOD(beginMultilineCommSingleLine)
 		{
-			vector<string>text = { "void main()","{","int a;//int t/*int k;","/*a=10;","a=2;*/","}" };
+			vector<string>text = { "void main()","{","int a;//int t/*int k;","}" };
 			int numberRow = 0;
 			int numberPos = -1;
 			int indexRowBegin = 0;
 			int posBegin = -1;
 			int countFindBegin = 0;
-			int expcountFindBegin = -1;
+			int expcountFindBegin = 1;
 			int expindexRowBegin = 0;
 			int expposBegin = -1;
 
@@ -72,7 +72,7 @@ namespace UnitTestfindBeginComment
 			int indexRowBegin = 0;
 			int posBegin = -1;
 			int countFindBegin = 0;
-			int expcountFindBegin = -1;
+			int expcountFindBegin = 1;
 			int expindexRowBegin = 0;
 			int expposBegin = -1;
 
